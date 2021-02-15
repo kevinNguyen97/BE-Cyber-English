47169797475c40fe1125765bf5f09472ee84db96
kevinNguyen97 <xaythixin@gmail.com>
2021-02-15 15:21:38 +0700

    implement structure

---
+++ src/config/config.ts
@@ -0,0 +1,36 @@
+import dotenv from 'dotenv'
+import myConfig from '../enviroments'
+
+dotenv.config();
+
+const MYSQL_HOST = process.env.MYSQL_HOST || myConfig.MYSQL_HOST || ""
+const MYSQL_DATABASE = process.env.MYSQL_DATABASE || myConfig.MYSQL_DATABASE || ""
+const MYSQL_USER = process.env.MYSQL_USER || myConfig.MYSQL_USER || ""
+const MYSQL_PASSWORD = process.env.MYSQL_PASSWORD || myConfig.MYSQL_PASSWORD || ""
+const MYSQL_PORT = process.env.MYSQL_PORT || myConfig.MYSQL_PORT || ""
+const MYSQL_CONNECTIONLIMIT = process.env.MYSQL_CONNECTIONLIMIT || myConfig.MYSQL_CONNECTIONLIMIT || 10
+
+const MYSQL = {
+    host: MYSQL_HOST,
+    database: MYSQL_DATABASE,
+    user: MYSQL_USER,
+    password: MYSQL_PASSWORD,
+    port: Number(MYSQL_PORT),
+    connectionLimit: Number(MYSQL_CONNECTIONLIMIT),
+}
+
+const SERVER_HOSTNAME = process.env.SERVER_HOSTNAME || 'localhost';
+const SERVER_PORT = process.env.SERVER_PORT || 3000;
+
+
+const SERVER = {
+    hostName: SERVER_HOSTNAME,
+    port: SERVER_PORT,
+}
+
+const config = {
+    mysql: MYSQL,
+    server: SERVER,
+}
+
+export default config;
---
+++ src/interfaces/types/type.ts
@@ -0,0 +1,2 @@
+export type stringOrNull = string | null;
+export type numberOrNull = number | null;
