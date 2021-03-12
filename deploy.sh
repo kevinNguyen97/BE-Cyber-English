5587986624903c194402ef09b962065a228299bb
kevinNguyen97 <xaythixin@gmail.com>
2021-03-12 15:32:42 +0700

    update serve file

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,9 @@
 
 pm2 stop BECyberEnglish
 pm2 stop serveFile
+pm2 delete serveFile
 npm install
 npm run build
-pm2 start serveFile
+pm2 serve public 3001 --name serveFile
 cd dist
-pm2 start BECyberEnglish
+pm2 start --name BECyberEnglish index.js
