d0f48d162d9cf851f9a80fd18cc26a2fccba5971
kevinNguyen97 <xaythixin@gmail.com>
2021-04-02 14:37:55 +0700

    change assets url

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,4 @@
-export const baseMediaUrl = "/assets/media/";
+export const baseMediaUrl = "/media/";
 export const baseAvatarUrl = "/img/avatar";
 export const cyberLearnFacebookLoginURI = '/quyen/checkuser'
 export const regex = {
