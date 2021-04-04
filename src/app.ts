1bf678e9285aa3791e90590206ffaf4cbaf552d7
kevinNguyen97 <xaythixin@gmail.com>
2021-04-05 00:18:51 +0700

    add assets path

--- src/app.ts
+++ src/app.ts
@@ -59,7 +59,7 @@ class AppRouter {
     });
 
     // static
-    this.appRouter.use(express.static("../public"));
+    this.appRouter.use("/assets",express.static("../public"));
 
     this.appRouter.use("/api/vocabulary", this.vocabulary.router);
     this.appRouter.use("/api/listening", this.listening.router);
